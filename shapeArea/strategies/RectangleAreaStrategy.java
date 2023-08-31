5e7ad19ada25043c49a73d4180cdd6fed2f8ddc8
feyzarafiq <feyza_rafiq@hotmail.de>
2023-08-31 07:33:19 +0200

    added tips

--- shapeArea/strategies/RectangleAreaStrategy.java
+++ shapeArea/strategies/RectangleAreaStrategy.java
@@ -1,2 +1,5 @@
 package strategies;
 
+// if you have a Shape object that you know is actually a Rectangle,
+// you can cast it to a rectangle to access
+// properties/methods specific to rectangles
