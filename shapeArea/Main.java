4d4e6d415f7e88b70a5fc8b2e20215bd45dad1a0
feyzarafiq <feyza_rafiq@hotmail.de>
2023-08-31 07:31:52 +0200

    added tips

--- shapeArea/Main.java
+++ shapeArea/Main.java
@@ -1,5 +1,3 @@
-
-
 public class Main {
     //write a code than runs an area calculation for a circle and a rectangle
     public static void main(String[] args) {
--- shapeArea/strategies/CircleAreaStrategy.java
+++ shapeArea/strategies/CircleAreaStrategy.java
@@ -1,6 +1,5 @@
 package strategies;
 
-
- //if you have a Shape object that you know is actually a Circle,
-    // you can cast it to a Circle to access
-    // properties/methods specific to circles 
+// if you have a Shape object that you know is actually a Circle,
+// you can cast it to a Circle to access
+// properties/methods specific to circles
